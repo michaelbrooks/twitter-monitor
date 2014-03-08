d7e68668c666d927a2003fbd204a8eb7a9d5ace5
Michael Brooks <mjbrooks@uw.edu>
2014-03-07 19:17:05 -0800

    add download_url

--- setup.py
+++ setup.py
@@ -2,20 +2,25 @@ from setuptools import setup
 
 setup(
     name="twitter-monitor",
+    packages=['twitter_monitor'],
+
     version="0.1.1",
+    download_url='https://github.com/michaelbrooks/twitter-monitor/archive/v0.1.1.zip',
+    url="https://github.com/michaelbrooks/twitter-monitor",
+
     author="Michael Brooks",
     author_email="mjbrooks@uw.edu",
+
     description=("A Twitter streaming library built on tweepy "
                  "that enables dynamic term tracking"),
     license="MIT",
     keywords=["twitter", "streaming", "tweepy", "filter"],
-    url="https://github.com/michaelbrooks/twitter-monitor",
-    packages=['twitter_monitor'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
     ],
+
     install_requires=[
         "tweepy >= 2.2"
     ],

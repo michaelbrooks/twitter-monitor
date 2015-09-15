3f99cea8492d3bdaa16f28a038bc8cf6022222ba
Michael Brooks <mjbrooks@uw.edu>
2015-09-15 15:38:53 -0700

    version 0.3.0

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-VERSION="0.2.10"
+VERSION="0.3.0"
 
 setup(
     name="twitter-monitor",
-    packages=['twitter_monitor'],
+    packages=["twitter_monitor"],
 
     version=VERSION,
-    download_url='https://github.com/michaelbrooks/twitter-monitor/archive/v%s.zip' % VERSION,
+    download_url="https://github.com/michaelbrooks/twitter-monitor/archive/v%s.zip" % VERSION,
     url="https://github.com/michaelbrooks/twitter-monitor",
 
     author="Michael Brooks",
@@ -15,13 +15,17 @@ setup(
 
     description=("A Twitter streaming library built on tweepy "
                  "that enables dynamic term tracking"),
-    long_description=open('README.md').read(),
+    long_description=open("README.md").read(),
     license="MIT",
     keywords=["twitter", "streaming", "tweepy", "filter"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+        
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
     ],
 
     install_requires=[
@@ -29,5 +33,5 @@ setup(
     ],
     test_suite="tests",
     tests_require=["mock == 1.0.1"],
-    scripts=['scripts/stream_tweets'],
+    scripts=["scripts/stream_tweets"],
 )
